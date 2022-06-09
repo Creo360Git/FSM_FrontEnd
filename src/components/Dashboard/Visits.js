5611dca540b27c7ccb57b27d0bac71069b818f06
Budhvin Thinomal <61777208+BudhvinThinomal@users.noreply.github.com>
2022-06-09 23:51:02 +0530

    Changes done

--- src/components/Dashboard/Visits.js
+++ src/components/Dashboard/Visits.js
@@ -15,7 +15,7 @@ import { styled } from "@mui/styles";
 // import { AuthContext } from "../../auth/AuthProvider";
 // import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import MuiDataTable from "./MuiDataTable";
+import MuiDataTable from "../Controls/MuiDataTable";
 
 const Visits = () => {
   const theme = useTheme();
